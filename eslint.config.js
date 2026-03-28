8b844f225485910cf81394e41abf97e0665c9d1a
Da3n0n <Da3n0n@users.noreply.github.com>
2026-03-28 18:04:55 +0000

    sync: 2026-03-28 18:04

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'warn',
+      'no-var': 'error',
+    },
+  },
+  eslint.configs.recommended,
+];
